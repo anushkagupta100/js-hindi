68d2f8bc2ec0ee1783b2407e4b85d2749c465819
anushkagupta100 <anushkag078@gmail.com>
2025-08-04 16:57:40 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3 + 3); // we are using nodejs, not browser
+
+console.log(3 + 3)
+console.log("anushka")
+
+//code readibility should be high 
+
+let name = "anushka"
+let age = 21
+let isLoggedIn = false
+
+// number = 2^53
+// bigint
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined = 
+// symbol = unique
+
+// object 
+
+console.log(typeof name);
+console.log(typeof undefined); //output= undefined
+console.log(typeof null); // output = object
